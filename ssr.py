ccfac02ba5044aef59525c76f66165661647e400
MOrowo <athipat.b@gmail.com>
2022-02-26 21:09:04 +0700

    Add files via upload

--- ssr.py
+++ ssr.py
@@ -14,41 +14,53 @@ while True:
             audio = r.listen(source)
         words = r.recognize_sphinx(audio)
         print(words)
+        color_list = ["test", "red", "green", "blue", "yellow", "exit"]
+        if words in color_list:
+            doColor(words)
+        else:
+            continue
 
-        if words == "test":
-            print("test has been executed")
-
-        if words == "red":
-            clear()
-            set_all(255,0,0)
-            show()
-
-        if words == "green":
-            clear()
-            set_all(0,255,0)
-            show()
-
-        if words == "blue":
-            clear()
-            set_all(0,0,255)
-            show()
-
-        if words == "yellow":
-            clear()
-            set_all(115, 115, 0)
-            show()
-
-        if words == "exit":
-            print("...")
-            sleep(1)
-            print("...")
-            sleep(1)
-            print("...")
-            sleep(1)
-            print("Goodbye")
-            break
-        
     except sr.UnknownValueError:
         print("Sphinx could not understand audio")
     except sr.RequestError as e:
         print("Sphinx error; {0}".format(e))
+
+
+def doColor(words):
+    if words == "test":
+        print("test has been executed")
+        return True
+
+    if words == "red":
+        clear()
+        set_all(255,0,0)
+        show()
+        return True
+
+    if words == "green":
+        clear()
+        set_all(0,255,0)
+        show()
+        return True
+
+    if words == "blue":
+        clear()
+        set_all(0,0,255)
+        show()
+        return True
+
+    if words == "yellow":
+        clear()
+        set_all(115, 115, 0)
+        show()
+        return True
+
+    if words == "exit":
+        print("...")
+        sleep(1)
+        print("...")
+        sleep(1)
+        print("...")
+        sleep(1)
+        print("Goodbye")
+        return False
